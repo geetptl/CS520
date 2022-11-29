2b3e2a90766167fe2ff53f7714301804742ebddf
hasnaingandhi <hasnain2804@gmail.com>
2022-11-28 22:14:05 -0500

    Updating perceptron branch with Perceptron.py file

--- hw2-classification/perceptron.py
+++ hw2-classification/perceptron.py
@@ -1,4 +1,4 @@
-# perceptron.py
+ # perceptron.py
 # -------------
 # Licensing Information: Please do not distribute or publish solutions to this
 # project. You are free to use and extend these projects for educational
@@ -8,7 +8,9 @@
 
 # Perceptron implementation
 import util
-
+import numpy as np
+import cv2
+import pandas as pd
 PRINT = True
 
 
@@ -19,19 +21,24 @@ class PerceptronClassifier:
     Note that the variable 'datum' in this code refers to a counter of features
     (not to a raw samples.Datum).
     """
-
+    
     def __init__(self, legalLabels, max_iterations):
         self.legalLabels = legalLabels
         self.type = "perceptron"
         self.max_iterations = max_iterations
-        self.weights = {}
+        self.weights = []
+        self.newWeights = np.zeros(shape=(len(legalLabels),784),dtype=np.int32)
+
         for label in legalLabels:
-            self.weights[label] = util.Counter()  # this is the data-structure you should use
+            #self.weights[label] = util.Counter()  # this is the data-structure you should use
+            #self.weights[label] = np.zeros(784)
+            print(" ")
 
     def setWeights(self, weights):
         assert len(weights) == len(self.legalLabels);
         self.weights == weights;
 
+    
     def train(self, trainingData, trainingLabels, validationData, validationLabels):
         """
         The training loop for the perceptron passes through the training data several
@@ -43,18 +50,46 @@ class PerceptronClassifier:
         datum is a counter from features to values for those features
         (and thus represents a vector a values).
         """
-
+        
         self.features = list(trainingData[0].keys())  # could be useful later
         # DO NOT ZERO OUT YOUR WEIGHTS BEFORE STARTING TRAINING, OR
         # THE AUTOGRADER WILL LIKELY DEDUCT POINTS.
-
+        learningRate = 0.1
+            #print(self.weights[label])
+        bias = 0
+        z = 0
+        
+        #self.weights = [[np.random.rand() for i in range(len(self.features))] for j in range(len(self.legalLabels))]
+        self.weights = np.random.rand(len(self.legalLabels),len(self.features))
+        #linearFuncCalculated = np.dot(data.items(),weights)                 
+        "*** YOUR CODE HERE ***"
+        NormalizedAccuracy = 0
         for iteration in range(self.max_iterations):
+            correctPred = 0
             print("Starting iteration ", iteration, "...")
-            for i in range(len(trainingData)):
-                "*** YOUR CODE HERE ***"
-                util.raiseNotDefined()
+            i = 0
+            for data,label in zip(trainingData,trainingLabels):
+                maxLabelVal = 0
+                predicted_value = 0
+                stepVal = 0
+                
+                i = 0
+                for y in range(len(self.legalLabels)):
+                    stepVal = np.dot(np.array(list(data.values())),self.weights[y])
+                    if(stepVal > maxLabelVal):
+                        maxLabelVal = stepVal
+                        predicted_value = y
+                if(predicted_value != label):
+                    self.weights[label] = self.weights[label] + np.array(list(data.values()))
+                    self.weights[predicted_value] = (self.weights[predicted_value]) - np.array(list(data.values()))
+                else:
+                    correctPred = correctPred + 1
+            accuracy = correctPred * 100 / len(trainingData)
+            NormalizedAccuracy = NormalizedAccuracy + accuracy
+        print("Accuracy:", NormalizedAccuracy/self.max_iterations)
+        
 
-    def classify(self, data):
+    def classify(self, data, labels="", runType = "validation"):
         """
         Classifies each datum as the label that most closely matches the prototype vector
         for that label.  See the project description for details.
@@ -65,9 +100,15 @@ class PerceptronClassifier:
         for datum in data:
             vectors = util.Counter()
             for l in self.legalLabels:
-                vectors[l] = self.weights[l] * datum
-            guesses.append(vectors.argMax())
+                vectors[l] = np.matmul(self.weights[l],np.array(list(datum.values())))
+
+            #guesses.append(vectors.argMax())
+            label = vectors.argMax()
+            guesses.append(label)
         return guesses
+    
+    def stepFunction(self,x):
+        return np.where(x>=0,1,0)
 
     def findHighWeightFeatures(self, label):
         """
@@ -76,6 +117,6 @@ class PerceptronClassifier:
         featuresWeights = []
 
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        #util.raiseNotDefined()
 
         return featuresWeights
